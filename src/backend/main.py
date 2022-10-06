859cd4b2cd9493e89d0d46983bf2dbf60c588e84
Sourcery AI <>
2022-10-06 14:29:41 +0000

    'Refactored by Sourcery'

--- src/backend/main.py
+++ src/backend/main.py
@@ -25,14 +25,16 @@ with Controller.from_port() as controller:
   # Create a hidden service where visitors of port 80 get redirected to local
   # port 5000 (this is where Flask runs by default).
 
-  print(" * Creating our hidden service in %s" % hidden_service_dir)
+  print(f" * Creating our hidden service in {hidden_service_dir}")
   result = controller.create_hidden_service(hidden_service_dir, 80, target_port = 5000)
 
   # The hostname is only available when we can read the hidden service
   # directory. This requires us to be running with the same user as tor.
 
   if result.hostname:
-    print(" * Our service is available at %s, press ctrl+c to quit" % result.hostname)
+    print(
+        f" * Our service is available at {result.hostname}, press ctrl+c to quit"
+    )
   else:
     print(" * Unable to determine our service's hostname, probably due to being unable to read the hidden service directory")
 
